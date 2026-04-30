4a7ec1aee57b1954d671e5a0c0fc8d7cf771b882
Beni Samuel <benisamuel566@gmail.com>
2026-04-30 15:06:35 +0200

    feat: added Jwt Filter per one request

--- src/main/java/com/intare_hotel/www/security/JwtAuthFilter.java
+++ src/main/java/com/intare_hotel/www/security/JwtAuthFilter.java
@@ -1,4 +1,51 @@
 package com.intare_hotel.www.security;
 
-public class JwtAuthFilter {
+import com.intare_hotel.www.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException
+    {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            try {
+                String email = jwtUtil.extractToken(token);
+                if (!email.isEmpty() && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+                    if (jwtUtil.isTokenValid(token, userDetails)) {
+                        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
+                }
+            } catch (JwtException e) {
+                System.out.println("Exception due to JWT issues");
+                System.out.println(e.getMessage());
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
