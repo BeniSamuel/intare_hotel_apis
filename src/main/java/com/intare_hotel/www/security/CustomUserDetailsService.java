92353e446c7b6abc1dfb47450055575405a8aadb
Beni Samuel <benisamuel566@gmail.com>
2026-04-30 15:07:13 +0200

    feat: added service to load userdetail security object

--- src/main/java/com/intare_hotel/www/security/CustomUserDetailsService.java
+++ src/main/java/com/intare_hotel/www/security/CustomUserDetailsService.java
@@ -1,4 +1,19 @@
 package com.intare_hotel.www.security;
 
-public class CustomUserDetailsService {
+import com.intare_hotel.www.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       return new CustomUserDetails(userService.getUserByEmail(username));
+    }
 }
